397d083c7f2d49a43b92ec24ae8dbcaaf3a84142
vagnersantosdasilva <vagnersantosdasilva@gmail.com>
2025-11-20 22:45:26 -0300

    feat:list plans on plans page

--- frontend/src/app/pages/planos-page/planos-page.ts
+++ frontend/src/app/pages/planos-page/planos-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { TableColumn, TableDataRow } from '../../shared/models/table.model';
+import { PlanosService } from '../../services/planos/planos';
 
 @Component({
   selector: 'app-planos-page',
@@ -6,6 +8,38 @@ import { Component } from '@angular/core';
   templateUrl: './planos-page.html',
   styleUrl: './planos-page.scss',
 })
-export class PlanosPage {
+
+export class PlanosPage implements OnInit {
+
+  public planosTable: { headers: TableColumn[]; data: TableDataRow[] } = { headers: [], data: [] }  ;
+  private planosService = inject(PlanosService);
+
+  ngOnInit(): void {
+     this.planosTable.headers = [
+      { header: 'ID', field: 'id' },
+      { header: 'Nome', field: 'nome'},
+      { header: 'Código ANS', field: 'codigo_registro_ans' },
+    ];
+    this.planosTable.data = [];
+    this.planosService.getPlanos().subscribe((planos) => {
+      this.planosTable.data = planos.map((plano) => ({
+        id: plano.id,
+        nome: plano.nome,
+        codigo_registro_ans: plano.codigo_registro_ans,
+      }));
+    });
+  }
+
+  public editPlano(row: TableDataRow): void {
+    console.log('Edit plan:', row);
+  }
+
+  public removePlano(row: TableDataRow): void {
+    console.log('Remove plan:', row);
+  }
+
+  public newPlano(): void {
+    console.log('New plan action triggered');
+  }
 
 }
